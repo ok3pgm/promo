f5e6fd5fa3b3333d9f6025f13add43bc38360dbd
ok3pgm <120733375+ok3pgm@users.noreply.github.com>
2023-06-10 22:29:13 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+    for (let i=0; i<$('.items_container input[type=checkbox]').length; i++) {
+        let text = $('.items_container input[type=checkbox]')[i].parentElement.getElementsByTagName('label')[0].textContent
+        const reg = /Небесная|Самоцвет|Купон/
+        if (reg.test(text)){
+            let check = $('.items_container input[type=checkbox]')[i]
+            check.checked=true;
+        }}
