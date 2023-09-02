74609ed55835f878e0f931d1ad0d12619e7e0711
ok3pgm <120733375+ok3pgm@users.noreply.github.com>
2023-09-02 22:09:11 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-    for (let i=0; i<$('.items_container input[type=checkbox]').length; i++) {
-        let text = $('.items_container input[type=checkbox]')[i].parentElement.getElementsByTagName('label')[0].textContent
-        const reg = /Небесная|Самоцвет|Купон/
-        if (reg.test(text)){
-            let check = $('.items_container input[type=checkbox]')[i]
-            check.checked=true;
-        }}
+for (let i=0; i<$('.items_container input[type=checkbox]').length; i++) {
+let text = $('.items_container input[type=checkbox]')[i].parentElement.getElementsByTagName('label')[0].textContent
+const reg = /Небесная|Самоцвет|Купон/
+if (reg.test(text)){
+let check = $('.items_container input[type=checkbox]')[i]
+check.checked=true;
+}}
+document..querySelector('[name="sum"]').click(); 
